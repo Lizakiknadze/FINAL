205f4549b8a1ef407446b13766a520f307b8dceb
Lizakiknadze <112972911+Lizakiknadze@users.noreply.github.com>
2022-12-02 23:43:52 +0400

    avoe

--- script.js
+++ script.js
@@ -1,53 +1,82 @@
-const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1"
-const IMG_PATH = `https://image.tmdb.org/t/p/w1280`
-const API_KEY = `d427831e0c241af4dae179660da6019b2`
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=%22"
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+const IMG_PATH = `https://image.tmdb.org/t/p/w1280`;
+const API_KEY = `d427831e0c241af4dae179660da6019b2`;
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=%22";
 
-const main = document.getElementById('main')
-const form = document.getElementById('inputs')
-const search = document.getElementById('search')
-const vote_average = document.getElementById('IMDB')
+const main = document.getElementById("main");
+const form = document.getElementById("inputs");
+const search = document.getElementById("search");
+const vote_average = document.getElementById("IMDB");
 
 getMovies(API_URL);
-async function getMovies(url){
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-    console.log(data.results)
-    showMovies(data.results)
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+  console.log(data.results);
+  showMovies(data.results);
 }
-function showMovies (movie){
-    movie.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
-        movieEl.innerHTML = `
+function showMovies(movie) {
+  movie.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
         <img src="${IMG_PATH + poster_path}" alt=${title}>
         <div class = 'movie-info'>
         <h3>${title}</h3>
     
-        `
-        main.appendChild(movieEl)
-    }); 
-}
+        `;
+    main.appendChild(movieEl);
+    const modal = document.getElementById("myModal");
+    const modalcontent = document.getElementById("modal-content");
+    const span = document.getElementsByClassName("close")[0];
+    movieEl.addEventListener("click", () => {
+      console.log(movie);
+      modal.style.display = "block";
 
+      span.onclick = () => {
+        modal.style.display = "none";
+      };
+      window.onclick = (event) => {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+      modalcontent.innerHTML = `
+    <img src="${IMG_PATH + poster_path}" alt="${title}">
+    <div class="movie-info">
+    <h3>${title}</h3>
     
-
+    </div>
+    <div class="overview">
+    <h3>Overview</h3>
+    ${overview}
+    </div>
+    `;
+    });
+  });
+}
 
 async function get_movie_by_search(search_term) {
-    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`)
-    const respData = await resp.json()
-    return respData.results
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`
+  );
+  const respData = await resp.json();
+  return respData.results;
 }
 
-btn.addEventListener('click', add_searched_movies_to_dom)
+// btn.addEventListener('click', add_searched_movies_to_dom)
 
 async function add_searched_movies_to_dom() {
-    const data = await get_movie_by_search(input.value)
+  const data = await get_movie_by_search(input.value);
 
-    main_grid_title.innerText = `Search Results`
-    main_grid.innerHTML = data.map(e => {
-        return `
+  main_grid_title.innerText = `Search Results`;
+  main_grid.innerHTML = data
+    .map((e) => {
+      return `
             <div class="card" data-id="${e.id}">
                 <div class="img">
                     <img src="${IMG_PATH + e.poster_path}">
@@ -64,40 +93,47 @@ async function add_searched_movies_to_dom() {
                     </div>
                 </div>
             </div>
-        `
-    }).join('')
+        `;
+    })
+    .join("");
 
-    const cards = document.querySelectorAll('.card')
-    add_click_effect_to_card(cards)
+  const cards = document.querySelectorAll(".card");
+  add_click_effect_to_card(cards);
 }
 
 // POPUP
 async function get_movie_by_id(id) {
-    const resp = await fetch(`https://api.themoviedb.org/3/discover/movie/${id}?api_key=${API_KEY}`)
-    const respData = await resp.json()
-    return respData
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/discover/movie/${id}?api_key=${API_KEY}`
+  );
+  const respData = await resp.json();
+  return respData;
 }
 async function get_movie_trailer(id) {
-    const resp = await fetch(`https://api.themoviedb.org/3/discover/movie/${id}/videos?api_key=${API_KEY}`)
-    const respData = await resp.json()
-    return respData.results[0].key
+  const resp = await fetch(
+    `https://api.themoviedb.org/3/discover/movie/${id}/videos?api_key=${API_KEY}`
+  );
+  const respData = await resp.json();
+  return respData.results[0].key;
 }
 
 async function show_popup(card) {
-    popup_container.classList.add('show-popup')
+  popup_container.classList.add("show-popup");
 
-    const movie_id = card.getAttribute('data-id')
-    const movie = await get_movie_by_id(movie_id)
-    const movie_trailer = await get_movie_trailer(movie_id)
+  const movie_id = card.getAttribute("data-id");
+  const movie = await get_movie_by_id(movie_id);
+  const movie_trailer = await get_movie_trailer(movie_id);
 
-    popup_container.style.background = `linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 1)), url(${image_path + movie.poster_path})`
+  popup_container.style.background = `linear-gradient(rgba(0, 0, 0, .8), rgba(0, 0, 0, 1)), url(${
+    image_path + movie.poster_path
+  })`;
 
-    popup_container.innerHTML = `
+  popup_container.innerHTML = `
             <span class="x-icon">&#10006;</span>
             <div class="content">
                 <div class="left">
                     <div class="poster-img">
-                        <img src="${image_path + movie.poster_path}" alt="">
+                        <img src="${IMG_PATH + movie.poster_path}" alt="">
                     </div>
                     <div class="single-info">
                         <span>Add to favorites:</span>
@@ -132,7 +168,9 @@ async function show_popup(card) {
                     <div class="genres">
                         <h2>Genres</h2>
                         <ul>
-                            ${movie.genres.map(e => `<li>${e.name}</li>`).join('')}
+                            ${movie.genres
+                              .map((e) => `<li>${e.name}</li>`)
+                              .join("")}
                         </ul>
                     </div>
                     <div class="overview">
@@ -145,25 +183,29 @@ async function show_popup(card) {
                     </div>
                 </div>
             </div>
-    `
-    const x_icon = document.querySelector('.x-icon')
-    x_icon.addEventListener('click', () => popup_container.classList.remove('show-popup'))
+    `;
+  const x_icon = document.querySelector(".x-icon");
+  x_icon.addEventListener("click", () =>
+    popup_container.classList.remove("show-popup")
+  );
 
-    const heart_icon = popup_container.querySelector('.heart-icon')
+  const heart_icon = popup_container.querySelector(".heart-icon");
 
-    const movie_ids = get_LS()
-    for (let i = 0; i <= movie_ids.length; i++) {
-        if (movie_ids[i] == movie_id) heart_icon.classList.add('change-color')
+  const movie_ids = get_LS();
+  for (let i = 0; i <= movie_ids.length; i++) {
+    if (movie_ids[i] == movie_id) heart_icon.classList.add("change-color");
+  }
+
+  heart_icon.addEventListener("click", () => {
+    if (heart_icon.classList.contains("change-color")) {
+      remove_LS(movie_id);
+      heart_icon.classList.remove("change-color");
+    } else {
+      add_to_LS(movie_id);
+      heart_icon.classList.add("change-color");
     }
-
-    heart_icon.addEventListener('click', () => {
-        if (heart_icon.classList.contains('change-color')) {
-            remove_LS(movie_id)
-            heart_icon.classList.remove('change-color')
-        } else {
-            add_to_LS(movie_id)
-            heart_icon.classList.add('change-color')
-        }
-        fetch_favorite_movies()
-    })
+    fetch_favorite_movies();
+  });
 }
+
+
